c220545550770f53c908edab48e575700f6ef28e
Tom von Clef <thomas.vonclef@gmail.com>
2015-07-10 11:10:33 -0400

    Made changes to eliminate tslint complaints.

--- app/view1/view1.ts
+++ app/view1/view1.ts
@@ -11,4 +11,6 @@ angular.module('myApp.view1', ['ngRoute'])
 
 .controller('View1Ctrl', [function() {
 
+    // controller here
+
 }]);
--- app/view1/view1_test.ts
+++ app/view1/view1_test.ts
@@ -1,5 +1,8 @@
 'use strict';
 
+declare var module; // TODO: figure why this isn't taken care of by the Jasmine typing files
+declare var inject; // TODO: figure why this isn't taken care of by the Jasmine typing files
+
 describe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
